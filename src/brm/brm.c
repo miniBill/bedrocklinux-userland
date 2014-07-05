9cab8a0aa4f23c01be8cfb1b43b10928bab6b4d6
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2014-07-05 20:01:40 +0200

    Formatting

--- src/brm/brm.c
+++ src/brm/brm.c
@@ -21,22 +21,22 @@
 #define dynarray(T) dynarray_##T
 
 #define inline_dynarray(T) { \
-    int size; \
-    T * array; \
+	int size; \
+	T * array; \
 }
 
 #define def_dynarray(T) struct dynarray(T) inline_dynarray(T);
 
 struct tables {
-    struct inline_dynarray(uid_t) uid;
-    struct inline_dynarray(gid_t) gid;
+	struct inline_dynarray(uid_t) uid;
+	struct inline_dynarray(gid_t) gid;
 };
 
 enum stat_result {
-    File,
-    Directory,
-    Other,
-    Nothing // Doesn't exist
+	File,
+	Directory,
+	Other,
+	Nothing // Doesn't exist
 };
 
 /*
@@ -44,27 +44,21 @@ enum stat_result {
  */
 enum stat_result lookup_path(const char* path)
 {
-    struct stat s;
+	struct stat s;
 
-    int err = stat(path, &s);
-
-    if(err == -1) {
-        if(errno != ENOENT) {
-            perror("stat");
-            exit(1);
-        }
-        return Nothing;
-    } else {
-        if(S_ISDIR(s.st_mode)) {
-            return Directory;
-        } else {
-            if(S_ISREG(s.st_mode)) {
-                return File;
-            } else {
-                return Other;
-            }
-        }
-    }
+	if (stat(path, &s) == -1) {
+		if (errno != ENOENT) {
+			perror("stat");
+			exit(1);
+		}
+		return Nothing;
+	}
+	if (S_ISDIR(s.st_mode)) {
+		return Directory;
+	} else if (S_ISREG(s.st_mode)) {
+		return File;
+	}
+	return Other;
 }
 
 void merge_etcfiles(const char* master, const char* client, struct tables* tables)
@@ -79,37 +73,37 @@ void walk_tree(const char* tree, struct tables* tables)
 
 int main(int argc, char* argv[])
 {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: brm <master etc> <client etc> [<client path>]\n");
-        fprintf(stderr, "The optional parameter, if supplied, enables owner fixing.\n");
+	if (argc < 3) {
+		fprintf(stderr, "Usage: brm <master etc> <client etc> [<client path>]\n");
+		fprintf(stderr, "The optional parameter, if supplied, enables owner fixing.\n");
 		exit(1);
 	}
 
-    const char* master = argv[1];
-    const char* client = argv[2];
-    const char* tree   = argv[3];
+	const char* master = argv[1];
+	const char* client = argv[2];
+	const char* tree   = argv[3];
 
-    if(lookup_path(master) != Directory) {
-        fprintf(stderr, "Master path doesn't exist or is not a directory");
-        exit(1);
-    }
+	if (lookup_path(master) != Directory) {
+		fprintf(stderr, "Master path doesn't exist or is not a directory");
+		exit(1);
+	}
 
-    if(lookup_path(client) != Directory) {
-        fprintf(stderr, "Client path doesn't exist or is not a directory");
-        exit(1);
-    }
+	if (lookup_path(client) != Directory) {
+		fprintf(stderr, "Client path doesn't exist or is not a directory");
+		exit(1);
+	}
 
-    if(tree && lookup_path(tree) != Directory) {
-        fprintf(stderr, "Tree path doesn't exist or is not a directory");
-        exit(1);
-    }
+	if (tree != NULL && lookup_path(tree) != Directory) {
+		fprintf(stderr, "Tree path doesn't exist or is not a directory");
+		exit(1);
+	}
 
-    struct tables tables;
-    merge_etcfiles(master, client, &tables);
+	struct tables tables;
+	merge_etcfiles(master, client, &tables);
 
-    if(tree) {
-        walk_tree(tree, &tables);
-    }
+	if (tree != NULL) {
+		walk_tree(tree, &tables);
+	}
 
 	return -1;
 }
